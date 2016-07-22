df6aee3e4096e52e09d98c66c562ce0e3bbd25bb
ryof <ryof@users.noreply.github.com>
2016-07-22 15:52:00 -0700

    Update initwin.cmd
    add installation of winscp

--- initwin.cmd
+++ initwin.cmd
@@ -1,8 +1,10 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco install -y atom
+choco install -y awscli
 rem fails somehow
 rem choco install -y cygwin
 choco install -y eclipse
+choco install -y git
 choco install -y googlechrome
 choco install -y jdk8
 choco install -y skype
@@ -10,5 +12,4 @@ choco install -y slack
 choco install -y transmission-qt
 choco install -y visualstudiocode
 choco install -y vlc
-choco install -y awscli
-choco install -y git
+choco install -y winscp
