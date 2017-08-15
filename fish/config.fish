fdbea28b692f0b72dfb9317f8499310355d79df7
ryof <ryo.furuyama@gmail.com>
2017-08-15 14:12:30 +0900

    add PATH configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
+
+set -x PATH $PATH /usl/local/bin /usr/local/sbin $HOME/.rbenv/bin $HOME/.pyenv/bin $HOME/.nodebrew/current/bin
