bb18d9ffecba1071b67d77acf3e600eee65ba6ac
ryof <ryo.furuyama@gmail.com>
2017-08-25 13:49:56 +0900

    add some alias and fix rbenv/pyenv configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,37 +1,13 @@
+alias fuck='networksetup -setairportpower en0 off; sleep 2; networksetup -setairportpower en0 on'
+alias less='less -N'
+alias ls='ls -G'
+
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
 
-set -x PATH $PATH /usr/local/bin /usr/local/sbin $HOME/.rbenv/shims $HOME/.pyenv/shims $HOME/.nodebrew/current/bin
+set -x PATH $PATH /usr/local/bin /usr/local/sbin $HOME/.nodebrew/current/bin
 
-# rbenv configuration
-set -gx RBENV_SHELL fish
-source '/usr/local/Cellar/rbenv/1.1.1/libexec/../completions/rbenv.fish'
-command rbenv rehash 2>/dev/null
-function rbenv
-  set command $argv[1]
-  set -e argv[1]
-
-  switch "$command"
-  case rehash shell
-    source (rbenv "sh-$command" $argv|psub)
-  case '*'
-    command rbenv "$command" $argv
-  end
-end
-
-# pyenv configuration
-set -gx PYENV_SHELL fish
-source '/usr/local/Cellar/pyenv/1.1.3/libexec/../completions/pyenv.fish'
-command pyenv rehash 2>/dev/null
-function pyenv
-  set command $argv[1]
-  set -e argv[1]
-
-  switch "$command"
-  case rehash shell
-    source (pyenv "sh-$command" $argv|psub)
-  case '*'
-    command pyenv "$command" $argv
-  end
-end
+# pyenv/rbenv configuration
+status --is-interactive; and source (pyenv init -|psub)
+status --is-interactive; and source (rbenv init -|psub)
